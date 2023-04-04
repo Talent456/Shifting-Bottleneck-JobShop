4176d930207e63ffac41c5c7aaf9e0d98ec625bd
Daniel Foot <d.foot@magenta.de>
2023-04-04 20:41:23 +0200

    Gui Mock Up

--- Factory/factoryGui.py
+++ Factory/factoryGui.py
@@ -2,4 +2,17 @@
 
 import PySimpleGUI as sg
 
-sg.Window(title="", layout=[[]], margins=(500, 200)).read()
+layout = [[sg.Text("Hier erstellen Sie die Factory")], [sg.Button("OK")]]
+
+# Create the window
+window = sg.Window("Factory Erstellen", layout, margins=(500, 200))
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
