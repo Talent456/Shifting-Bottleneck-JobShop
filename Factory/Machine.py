dee5ae79e0f5e770100000b751239d704e358f85
Talent456 <d.foot@magenta.de>
2023-07-18 18:24:39 +0200

    Getting it Ready to Screenshot

--- Factory/Machine.py
+++ Factory/Machine.py
@@ -1,15 +1,6 @@
-from ast import List
-
-
 class Machine:
     # Jede Machine hat eine ID und die Knoten/Jobschritte die darauf ausgeführt werden 
 
-    def __init__(self, id, nodes:List):
+    def __init__(self, id, nodes):
         self.id = id
         self.nodes = nodes
-
-    def getNodes(self):
-        return self.nodes
-    
-    def getId(self):
-        return self.id
--- Factory/job.py
+++ Factory/job.py
@@ -1,5 +1,6 @@
 class Job:
-    # Jeder Job hat eine id, eine Reihenfolge(Liste) von Maschinen und die Bearbeitungsdauer pro Schritt(Liste)
+    # Jeder Job hat eine id, eine Reihenfolge(Liste) von Maschinen
+    # und die Bearbeitungsdauer pro Schritt(Liste)
 
     def __init__(self, id, machines, processingTime):
         self.id = id
